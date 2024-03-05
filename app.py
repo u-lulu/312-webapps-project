91c5c09d6fbf3332dcfc502ff5924d65cad0b1ba
ululu <mtmerlino@gmail.com>
2024-03-04 20:50:20 -0500

    security: set nosniff on all responses

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
-from flask import Flask, send_file, abort
+from flask import Flask, send_file, abort, make_response
 import os
 #python -m flask run
 
 app = Flask(__name__)
 
+def serve_file(path):
+    response = make_response(send_file(path))
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
 @app.route("/")
 def homepage():
-    return send_file("TEST_webpage.html")
+    return serve_file("TEST_webpage.html")
 
 @app.route("/styles.css")
 def css():
-    return send_file("styles.css")
+    return serve_file("styles.css")
 
 @app.route("/scripts/<name>.js")
 def get_js(name):
     if os.path.exists("scripts/" + name + ".js"):
-        return send_file("scripts/" + name + ".js")
+        return serve_file("scripts/" + name + ".js")
     else:
         return abort(404)
 
 @app.route("/favicon.ico")
 def favicon():
-    return send_file("favicon.ico")
+    return serve_file("favicon.ico")
 
 @app.route("/images/<filename>")
 def get_image(filename):
     if os.path.exists("images/" + filename):
-        return send_file("images/" + filename)
+        return serve_file("images/" + filename)
     else:
         return abort(404)
